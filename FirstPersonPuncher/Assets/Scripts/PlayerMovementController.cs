2a56f1c5c653fb9fc1341583561adc6bdef51802
antronik7 <antoineberthiaumedutil@gmail.com>
2024-04-15 22:25:22 -0400

    Auto Step
    -Added a function to move up the character when in from of an elevation (this causes a bug with slopes).

--- FirstPersonPuncher/Assets/Scripts/PlayerMovementController.cs
+++ FirstPersonPuncher/Assets/Scripts/PlayerMovementController.cs
@@ -6,14 +6,18 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] Transform characterOrientation;
     [SerializeField] float movementSpeed = 1f;
+    [SerializeField] float maxAutoStepHeight = 0.5f;
+    [SerializeField] float stepSpeed = 1f;
 
     private Rigidbody rb;
+    private CapsuleCollider col;
     private JumpController jumpController;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
         jumpController = GetComponent<JumpController>();
     }
 
@@ -24,6 +28,9 @@ public class PlayerMovementController : MonoBehaviour
 
         Vector3 movementDirection = characterOrientation.forward * vertical + characterOrientation.right * horizontal;
         movementDirection = Vector3.ClampMagnitude(movementDirection, 1f);
+
+        AutoStep(movementDirection);
+
         Vector3 movementVelocity = movementDirection * movementSpeed;
 
         if (jumpController.getIsGrounded() && jumpController.getGroundAngle() > 0f)
@@ -32,5 +39,14 @@ public class PlayerMovementController : MonoBehaviour
         movementVelocity.y += jumpController.getYVelocity();
 
         rb.velocity = movementVelocity;
+    }
+
+    private void AutoStep(Vector3 direction)
+    {
+        direction.Normalize();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position - (Vector3.up * (col.height/2f - 0.01f)), direction, out hit, col.radius + 0.01f))
+            if (Physics.Raycast(transform.position - (Vector3.up * (col.height / 2f - maxAutoStepHeight)) + (direction * (col.radius + 0.01f)), Vector3.down, out hit, maxAutoStepHeight - 0.01f))
+                rb.position += new Vector3 (0f, stepSpeed * Time.deltaTime, 0f);
     }
 }
